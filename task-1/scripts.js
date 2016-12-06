00530e9877e20a8e551e5345c94ead2823f6f07a
TamTania <solovei_tania@ukr.net>
2016-12-06 23:50:54 +0200

    Added task #1

---
+++ task-1/scripts.js
@@ -0,0 +1,6 @@
+var name = prompt('What`s your name', 'Name');
+var time = prompt('When the event will start?', '18:00');
+var place = prompt('Where the event will pass?', 'work');
+var meeting = prompt('What type of event it will be?', 'meeting');
+
+console.log (name + ' has a ' + meeting + ' today at ' +  time + ' at ' + place);
