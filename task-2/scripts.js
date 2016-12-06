302aac3a0afbcfe7f180c91fdaad6376620601ee
TamTania <solovei_tania@ukr.net>
2016-12-07 00:30:35 +0200

    Addedd task #2

---
+++ task-2/scripts.js
@@ -0,0 +1,6 @@
+var x1 = 3, y1 = 3;
+var x2 = prompt('Enter x2 value');
+var y2 = prompt('Enter y2 value')
+var distance = Math.sqrt(Math.pow((x2-x1), 2) + Math.pow((y2-y1), 2));
+
+console.log('The distance between points A and B equals ' + distance); 
