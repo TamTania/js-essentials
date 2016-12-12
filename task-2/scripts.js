5b708a5405d9fe439326188ddf2cd34d84a91d77
TamTania <solovei_tania@ukr.net>
2016-12-12 17:11:36 +0200

    fixed alert in task #2

--- task-2/scripts.js
+++ task-2/scripts.js
@@ -1,6 +1,6 @@
 var x1 = 3, y1 = 3;
-var x2 = prompt('Enter x2 value');
-var y2 = prompt('Enter y2 value')
+var x2 = prompt('Enter X value');
+var y2 = prompt('Enter Y value')
 var distance = Math.sqrt(Math.pow((x2-x1), 2) + Math.pow((y2-y1), 2));
 
-console.log('The distance between points A and B equals ' + distance); 
+alert('The distance between points A(' + x1 + ', ' + x2 + ') and B(' + y1 + ', ' + y2 + ') equals '  + distance); 
